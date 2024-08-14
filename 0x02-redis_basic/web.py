523469eacbc1e8dc89873eca8bd30f5c1b5d2a9a
yafiet231 <abieleyasu893@gmail.com>
2024-08-14 15:15:42 +0000

    set

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-"""
-In this task, we will implement a get_page function
+"""In this tasks, we will implement a get_page function
 (prototype: def get_page(url: str) -> str:). The core of
 the function is very simple. It uses the requests module
 to obtain the HTML content of a particular URL and returns it.
@@ -13,46 +12,43 @@ accessed in the key "count:{url}" and cache the result with
 an expiration time of 10 seconds.
 
 Tip: Use http://slowwly.robertomurray.co.uk to simulate
-a slow response and test your caching.
-"""
+a slow response and test your caching."""
+
 
 import redis
 import requests
 from functools import wraps
 
-# Connect to Redis
 r = redis.Redis()
 
+
 def url_access_count(method):
-    """Decorator for get_page function"""
+    """decorator for get_page function"""
     @wraps(method)
     def wrapper(url):
-        """Wrapper function"""
+        """wrapper function"""
         key = "cached:" + url
-        key_count = "count:" + url
-
-        # Increment the access count
-        r.incr(key_count)
-
-        # Check if the URL is in the cache
         cached_value = r.get(key)
         if cached_value:
             return cached_value.decode("utf-8")
 
-        # Get new content and update cache
+            # Get new content and update cache
+        key_count = "count:" + url
         html_content = method(url)
-        r.setex(key, 10, html_content)
+
+        r.incr(key_count)
+        r.set(key, html_content, ex=10)
+        r.expire(key, 10)
         return html_content
     return wrapper
 
+
 @url_access_count
 def get_page(url: str) -> str:
-    """Obtain the HTML content of a particular URL"""
-    response = requests.get(url)
-    return response.text
+    """obtain the HTML content of a particular"""
+    results = requests.get(url)
+    return results.text
+
 
 if __name__ == "__main__":
-    url = 'https://postman-echo.com/delay/3'
-    print(get_page(url))
-    print(get_page(url))
-    print(get_page(url))
+    get_page('http://slowwly.robertomurray.co.uk')
