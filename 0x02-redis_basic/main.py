c0c17ab1d32633a9fd8b92065fba9709b2b6d819
yafiet231 <abieleyasu893@gmail.com>
2024-08-14 15:09:26 +0000

    form

--- 0x02-redis_basic/main.py
+++ 0x02-redis_basic/main.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python3
-""" Main file """
+"""
+main.py - Test script for web.py
+"""
 
-Cache = __import__('exercise').Cache
+from web import get_page
 
-cache = Cache()
-
-s1 = cache.store("first")
-print(s1)
-s2 = cache.store("secont")
-print(s2)
-s3 = cache.store("third")
-print(s3)
-
-inputs = cache._redis.lrange("{}:inputs".format(cache.store.__qualname__), 0, -1)
-outputs = cache._redis.lrange("{}:outputs".format(cache.store.__qualname__), 0, -1)
-
-print("inputs: {}".format(inputs))
-print("outputs: {}".format(outputs))
+if __name__ == "__main__":
+    url = 'http://slowwly.robertomurray.co.uk'
+    print(get_page(url))
+    print(get_page(url))
+    print(get_page(url))
--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""In this tasks, we will implement a get_page function
+"""
+In this task, we will implement a get_page function
 (prototype: def get_page(url: str) -> str:). The core of
 the function is very simple. It uses the requests module
 to obtain the HTML content of a particular URL and returns it.
@@ -12,43 +13,46 @@ accessed in the key "count:{url}" and cache the result with
 an expiration time of 10 seconds.
 
 Tip: Use http://slowwly.robertomurray.co.uk to simulate
-a slow response and test your caching."""
-
+a slow response and test your caching.
+"""
 
 import redis
 import requests
 from functools import wraps
 
+# Connect to Redis
 r = redis.Redis()
 
-
 def url_access_count(method):
-    """decorator for get_page function"""
+    """Decorator for get_page function"""
     @wraps(method)
     def wrapper(url):
-        """wrapper function"""
+        """Wrapper function"""
         key = "cached:" + url
+        key_count = "count:" + url
+
+        # Increment the access count
+        r.incr(key_count)
+
+        # Check if the URL is in the cache
         cached_value = r.get(key)
         if cached_value:
             return cached_value.decode("utf-8")
 
-            # Get new content and update cache
-        key_count = "count:" + url
+        # Get new content and update cache
         html_content = method(url)
-
-        r.incr(key_count)
-        r.set(key, html_content, ex=10)
-        r.expire(key, 10)
+        r.setex(key, 10, html_content)
         return html_content
     return wrapper
 
-
 @url_access_count
 def get_page(url: str) -> str:
-    """obtain the HTML content of a particular"""
-    results = requests.get(url)
-    return results.text
-
+    """Obtain the HTML content of a particular URL"""
+    response = requests.get(url)
+    return response.text
 
 if __name__ == "__main__":
-    get_page('http://slowwly.robertomurray.co.uk')
+    url = 'https://postman-echo.com/delay/3'
+    print(get_page(url))
+    print(get_page(url))
+    print(get_page(url))
