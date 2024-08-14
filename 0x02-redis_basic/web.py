f5803e273984fa2fbc55cdee95bcce4161bcb804
yafiet231 <abieleyasu893@gmail.com>
2024-08-14 13:33:00 +0000

    add

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,9 +6,11 @@ import requests
 from functools import wraps
 from typing import Callable
 
+
 redis_store = redis.Redis()
 '''The module-level Redis instance.
 '''
+
 
 def data_cacher(method: Callable) -> Callable:
     '''Caches the output of fetched data.
@@ -22,9 +24,11 @@ def data_cacher(method: Callable) -> Callable:
         if result:
             return result.decode('utf-8')
         result = method(url)
+        redis_store.set(f'count:{url}', 0)
         redis_store.setex(f'result:{url}', 10, result)
         return result
     return invoker
+
 
 @data_cacher
 def get_page(url: str) -> str:
